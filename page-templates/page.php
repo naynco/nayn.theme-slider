4cbf50f4e4dd6d51ab397c9444afa32f75747592
Hüseyin Mert <huseyinin@gmail.com>
2019-10-12 09:02:28 +0300

    unused code

--- page-templates/page.php
+++ page-templates/page.php
@@ -1,22 +1,3 @@
-<?php
-/**
- * Default page
- */
-global $options, $theCategory;
-
-// Get the author info
-$authorID = get_the_author_meta('ID');
-$postCategories   = get_the_category();
-$theCategory      = array();
-$authorDisplayName= get_the_author_meta('display_name', $authorID);
-$authorProfileURL = get_author_posts_url( $authorID, get_the_author_meta( 'user_nicename', $authorID ) );
-
-foreach ($postCategories as $category) {
-  $theCategory[] = $category->term_id;
-}
-
-?>
-
 <div class="hero-slider single-page">
   <div class="item hero-slider-item">
     
